6fb50ddd054f68d6dcf90b360bec31e83dedcd70
mr. whalen <michael.whalen@sitechhs.com>
2026-02-03 13:59:29 -0500

    pushed

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,32 +1,8 @@
 <template>
-  <div class="container">
-    <h1>Cookie Clicker</h1>
-    <h2>{{ count }}</h2>
-    <img @click="increment" src="/cookie.png" alt="" />
-  </div>
+  <div></div>
+  <RouterView />
 </template>
 
-<script setup>
-import { ref } from 'vue'
+<script setup></script>
 
-const count = ref(0)
-function increment() {
-  count.value++
-}
-</script>
-
-<style scoped>
-img {
-  width: 200px;
-  height: auto;
-  display: block;
-  margin-top: 20px;
-}
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 40vh;
-}
-</style>
+<style scoped></style>
