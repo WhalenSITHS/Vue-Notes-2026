7365db09196315cc439c0b205e14725184faad7a
mr. whalen <michael.whalen@sitechhs.com>
2026-02-04 12:23:59 -0500

    sdfg

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CookieClicker from '@/views/CookieClicker.vue'
+import UserCreate from '@/views/UserCreate.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: CookieClicker,
+    },
+    {
+      path: '/user-create',
+      name: 'user-create',
+      component: UserCreate,
     },
   ],
 })
---
+++ app/src/views/UserCreate.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <form action="submit" @submit.prevent="login(user)">
+      <label for="username">Username</label>
+      <input type="text" v-model="user.username" />
+      <label for="password">Password</label>
+      <input type="password" v-model="user.password" />
+    </form>
+  </div>
+</template>
+
+<script setup>
+//add new ref for loggedIn: Boolean. new function to update that value
+import { reactive, ref } from 'vue'
+const user = reactive({ username: '', password: '' })
+</script>
+
+<style lang="scss" scoped></style>
