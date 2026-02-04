fae1b0769016d4885ce956983a11b4b623743fdf
mr. whalen <michael.whalen@sitechhs.com>
2026-02-04 13:58:48 -0500

    asdf

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CookieClicker from '@/views/CookieClicker.vue'
+import UserCreate from '@/views/UserCreate.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -7,6 +8,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: CookieClicker,
+    },
+    {
+      path: '/Usercreate',
+      name: 'usercreate',
+      component: UserCreate,
     },
   ],
 })
---
+++ app/src/views/UserCreate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <form action="submit" @submit.prevent="login(user)">
+      <label for="username">Username</label>
+      <input type="text" v-model="user.username" />
+      <label for="password">password</label>
+      <input type="password" v-model="user.password" />
+      <button type="submit">Login</button>
+    </form>
+    <h2 v-if="loggedIn">{{ user.username }}</h2>
+    <h2 v-else>Please Login</h2>
+  </div>
+</template>
+
+<script setup>
+//create variable loggedIn = false, create finction login console log user and changed loggedIn to true
+import { ref, reactive } from 'vue'
+const user = reactive({ username: '', password: '' })
+const loggedIn = ref(false)
+function login(user) {
+  console.log(user)
+  loggedIn.value = true
+}
+</script>
+
+<style lang="scss" scoped></style>
